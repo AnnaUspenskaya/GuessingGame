fa718d19cf96a56aeb7b1741bc00faccd26c362a
Anna <anna.uspenskaya.5289@gmail.com>
2021-10-19 20:05:17 -0400

    guessing game vanilla JS

---
+++ scripts/guessinggame.js
@@ -0,0 +1,125 @@
+
+
+let select = Math.floor(Math.random() * 20);
+let buttons = document.getElementsByClassName("num");
+
+let nums = [];
+let count = 0;
+let going;
+let ran;
+let check;
+let deposit;
+let win;
+
+document.getElementById("numbers").disabled = true;
+
+function game() {
+    for (let i = 0; i <= 20; i++) {
+        buttons[i].disabled = false;
+
+    }
+    buttons[0].disabled = false;
+    deposit = prompt("How much are you depositing?");
+    do {
+        
+        if (isNaN(deposit)) {
+            deposit = prompt("How much are you depositing? \n Only numbers");
+        }
+        if (deposit == "") {
+            deposit = prompt("Make a deposit to begin the game \n How much are you depositing?  ");
+        }
+        if(deposit<=0){
+            deposit = prompt("insufficient fund.\n How much are you depositing?  ");
+        }
+    } while (isNaN(deposit) || deposit == "" || deposit <=0 );
+
+    document.getElementById('deposit').innerHTML = "Your deposit is: $" + deposit;
+    document.getElementById("start").style.display = "none";
+    document.getElementById("money").style.visibility = "visible";
+
+
+    for (let i = 0; i <= 20; i++) {
+
+        // do {
+        //     n = Math.floor(Math.random() * (max - min + 1)) + min;
+        //     p = numbers.includes(n);
+        //     if(!p){
+        //       numbers.push(n);
+        //     }
+        //   }
+        //   while(p);
+
+        do {
+            ran = Math.floor(Math.random() * 100);
+            check = nums.includes(ran);
+            if (!check) {
+                nums.push(ran);
+            }
+        }
+        while (check);
+
+        let ids = nums[i];
+        document.getElementsByClassName("num")[i].setAttribute("id", ids);
+        //document.getElementById(ids).innerHTML=nums[i];
+        buttons[i].innerHTML = nums[i];
+    }
+
+
+    console.log("nums: " + nums);
+    console.log("select: " + select);
+1`3`    
+}
+function guess(id) {
+    // do{
+    if (id != nums[select]) {
+        document.getElementById(id).style.background = "red";
+        count++;
+        document.getElementById(id).disabled = true;
+        console.log("no");
+        console.log("count: " + count);
+    } else {
+        document.getElementById(id).style.background = "green";
+        count++;
+        for (let i = 0; i < 20; i++) {
+            buttons[i].disabled = true;
+
+        } winning();
+
+        console.log("yes");
+        console.log("count: " + count);
+
+    }
+}
+//your winnings 
+function winning() {
+
+    switch (count) {
+        case 1:
+            win = deposit * 4;
+            break;
+        case 2:
+            win = deposit * 3.5;
+            break;
+        case 3:
+            win = deposit * 3;
+            break;
+        case 4:
+            win = deposit * 2.5;;
+        case 5: 
+            win = deposit * 2;
+        case 6:
+            win = deposit * 1.5;;
+        case 7:
+            win = deposit;
+        default:
+            win = deposit - (deposit / 20 * (count - 1));
+    }
+    win=Math.round((win  * 100) / 100)             jkbnjp\grt;
+    document.getElementById("win").style.visibility = "visible";
+
+    document.getElementById("winnings").innerHTML = "Congratulation! You've guessed the number! <h3> Your Winnings $" + win + '</h3>';
+    console.log("win: " + win);
+}
+
+
+
